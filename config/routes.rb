bdabdd660bf77ede9bc3f1344035095c923fd55f
kaichiroiida <s15mm302@gmail.com>
2017-07-25 05:12:26 +0000

    devise:viewを実行した

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :gee_mails
+  root 'gee_mails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
